95631d610e499b89a3ccca05feb24429d33571e4
mohammedhasan-prog <mohammed.hasan22b@iiitg.ac.in>
2025-02-09 19:29:39 +0530

    commit mongo connect

--- Backend/app.js
+++ Backend/app.js
@@ -1,5 +1,9 @@
 const express=require('express');
 const app=express();
+const dotenv=require('dotenv');
+const cors=require('cors');
+dotenv.config();
+app.use(cors());
 
 
 app.get('/',(req,res)=>{
--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,10 @@
 const http=require('http');
 const app=require('./app');
-const dotenv=require('dotenv');
 
-dotenv.config();
 const server=http.createServer(app);
+const port =process.env.PORT || 3000;
 
 
-server.listen(3000,()=>{
-    console.log('Server is running on port 3000');
+server.listen(port,()=>{
+    console.log('Server is running on port '+port);
 }   );
